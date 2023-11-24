e46a816e9e4481e7aa5bd76ba3dfe178ec31d4e1
Tarun Nahak <tarunnahak25@gmail.com>
2023-11-24 16:39:52 +0530

    Added the group routes

--- main.go
+++ main.go
@@ -10,12 +10,17 @@ func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"message": "Working just fine..."})
 }
 
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "Tarun Nahak"})
+}
+
 func main() {
 
 	app := fiber.New()
 
-	
+	apiV1 := app.Group("api/v1")
 	app.Get("/foo", handleFoo)
+	apiV1.Get("/user", handleUser)
 	log.Fatal(app.Listen(":3000"))
 
 }
