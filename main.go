ebe7ca6d2530d0fd63dbffb06b08f08a6cc8a099
Tarun Nahak <tarunnahak25@gmail.com>
2023-11-24 16:28:32 +0530

    Added server and handler

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func handleFoo(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"message": "Working just fine..."})
+}
+
+func main() {
+
+	app := fiber.New()
+	app.Get("/foo", handleFoo)
+	log.Fatal(app.Listen(":3000"))
+
+}
